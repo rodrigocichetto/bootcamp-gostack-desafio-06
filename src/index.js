10a074b4338d2a55a419b8f57904ba84b69ed31e
Rodrigo Cichetto <rodrigocichetto@gmail.com>
2019-11-13 13:03:59 -0300

    add react navigation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+// import { View } from 'react-native';
 
 import './config/ReactotronConfig';
+import Routes from './routes';
 
 const App = () => {
-  return <View />;
+  return <Routes />;
 };
 
 export default App;
